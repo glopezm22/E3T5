a3d02e4fba6a671bfd00ad78372e563cee16570c
rodrii20 <albarodriguez207@gmail.com>
2025-03-17 12:30:35 +0100

    menu bezero con el menu, botones y funcionalidad de botones en cpu, ram y videoCard, falta la funcionalidad del boton de atzera, los botones motherBoard, storage, produktu guztiak y las paginas de los submenus(datu pertsonalak, historiala y produktuak)

---
+++ src/erronka/DatuBasea.java
@@ -0,0 +1,146 @@
+package Aplikazioa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import Ariketak.Produktua;
+import Ariketak.Produktuak;
+
+import java.sql.SQLException;
+
+public class DatuBasea {
+	private String url;
+	   private String user;
+	   private String pass;
+	   
+	   //defektuzko sortzailea 
+	   public DatuBasea (){
+	      this.url="jdbc:oracle:thin:@localhost:1521/XEPDB1";
+	      this.user="GAMESTOP";
+	      this.pass="GAMESTOP";
+	   }
+	   
+	   //sortzailea parametroekin
+	   public DatuBasea (String url, String user, String pass){
+	      this.url=url;
+	      this.user=user;
+	      this.pass=pass;
+	   }
+	   
+	   
+	   //konexioa egin, bat generikoa balio izango duena konexio guztietarako
+	   public Connection konexioa (){
+	      Connection conn = null;
+	      try{
+	         conn = DriverManager.getConnection(url, user, pass);
+	         return conn;
+	      } catch (Exception e){
+	         System.out.println("Konexio errorea: "+e);
+	      }
+	      return conn;
+	   }
+
+	   ///HEMEN kontsultak, metedo bat kontsulta bakoitzeko, gogoratu konexioak itxi behar direla   
+	   public BDproduktu produktuakCPU() {
+		    BDproduktu pDB = new BDproduktu();
+		   
+		    try {
+		    	Connection conn = konexioa();
+		        Statement stmt = conn.createStatement();
+		        ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUKTU WHERE ID_KATEGORIA =1");
+
+		        while (rs.next()) {
+		            // Crear un objeto 'Produktu' con los datos de la base de datos
+		        	int id = rs.getInt("ID");
+		            String izena = rs.getString("IZENA");
+		            String deskribapena = rs.getString("DESKRIBAPENA");
+		            double balioa = rs.getDouble("BALIOA");
+		            double salneurria = rs.getDouble("SALNEURRIA");
+		            int idKategoria = rs.getInt("ID_KATEGORIA");
+		           
+		            // Agregar el producto a la lista
+			        Produktu produktua = new Produktu(id, izena, deskribapena, balioa, salneurria, idKategoria);
+		            pDB.produktuGehitu(produktua);
+		            
+		        }
+		      
+		    }catch (SQLException e) {
+		        System.out.println("Error al conectar o ejecutar la consulta.");
+		        e.printStackTrace();
+		    }  
+		    
+		    
+		    // Devolver la instancia de BDproduktu con los productos cargados
+		    return pDB;
+		}
+	   
+	   
+	   
+	   public BDproduktu produktuakRAM() {
+		    BDproduktu pDB = new BDproduktu();
+		   
+		    try {
+		    	Connection conn = konexioa();
+		        Statement stmt = conn.createStatement();
+		        ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUKTU WHERE ID_KATEGORIA =3");
+
+		        while (rs.next()) {
+		            // Crear un objeto 'Produktu' con los datos de la base de datos
+		        	int id = rs.getInt("ID");
+		            String izena = rs.getString("IZENA");
+		            String deskribapena = rs.getString("DESKRIBAPENA");
+		            double balioa = rs.getDouble("BALIOA");
+		            double salneurria = rs.getDouble("SALNEURRIA");
+		            int idKategoria = rs.getInt("ID_KATEGORIA");
+		           
+		            // Agregar el producto a la lista
+			        Produktu produktua = new Produktu(id, izena, deskribapena, balioa, salneurria, idKategoria);
+		            pDB.produktuGehitu(produktua);
+		            
+		        }
+		      
+		    }catch (SQLException e) {
+		        System.out.println("Error al conectar o ejecutar la consulta.");
+		        e.printStackTrace();
+		    }  
+		    
+		    
+		    // Devolver la instancia de BDproduktu con los productos cargados
+		    return pDB;
+		}
+	   
+	   public BDproduktu produktuakVideoCard() {
+		    BDproduktu pDB = new BDproduktu();
+		   
+		    try {
+		    	Connection conn = konexioa();
+		        Statement stmt = conn.createStatement();
+		        ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUKTU WHERE ID_KATEGORIA =2");
+
+		        while (rs.next()) {
+		            // Crear un objeto 'Produktu' con los datos de la base de datos
+		        	int id = rs.getInt("ID");
+		            String izena = rs.getString("IZENA");
+		            String deskribapena = rs.getString("DESKRIBAPENA");
+		            double balioa = rs.getDouble("BALIOA");
+		            double salneurria = rs.getDouble("SALNEURRIA");
+		            int idKategoria = rs.getInt("ID_KATEGORIA");
+		           
+		            // Agregar el producto a la lista
+			        Produktu produktua = new Produktu(id, izena, deskribapena, balioa, salneurria, idKategoria);
+		            pDB.produktuGehitu(produktua);
+		            
+		        }
+		      
+		    }catch (SQLException e) {
+		        System.out.println("Error al conectar o ejecutar la consulta.");
+		        e.printStackTrace();
+		    }  
+		    
+		    
+		    // Devolver la instancia de BDproduktu con los productos cargados
+		    return pDB;
+		}
+}
